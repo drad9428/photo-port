1845e7de19c7b4c2f31f3de0933f260c34a5faf2
drad <robertjdstoddard@gmail.com>
2021-09-29 23:10:46 -0600

    more tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,17 +1,19 @@
-import React from "react";
-import {render, cleanup} from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import About from '..'
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
-afterEach(cleanup)
+afterEach(cleanup);
 
 describe('About component', () => {
-    it('renders', () => {
-        render(<About />)
-    })
-
-    it('matches snapshot DOM node structure', () => {
-        const {asFragment} = render(<About />)
-        expect(asFragment()).toMatchSnapshot()
-    })
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
